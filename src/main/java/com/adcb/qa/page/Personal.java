473b2a797f4b996dfe8a0ec2b8503ddab78286b8
danyal <syeddanyalhassan@gmail.com>
2022-10-28 17:22:58 +0400

    new update in Personal class

--- src/main/java/com/adcb/qa/page/Personal.java
+++ src/main/java/com/adcb/qa/page/Personal.java
@@ -1,9 +1,29 @@
 package com.adcb.qa.page;
 
-public class Personal {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-	public Personal() {
+import com.adcb.qa.base.BasePage;
+
+public class Personal extends BasePage {
+
+	@FindBy(xpath="//label[@class='menu-bar']")
+	WebElement menu;
+	
+	@FindBy(xpath="//a[@aria-label='Business']")
+	WebElement business;
+	
+	public Personal(WebDriver driver) {
 		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver, this);
 	}
+	
+	public String getWebPageTitle()
+	{
+		return driver.getTitle();
+	}
+	
 
 }
